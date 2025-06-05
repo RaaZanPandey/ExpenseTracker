3febdd57aa48b77ff403e197ba71225e3be70183
Rajan Pandey <raazanpandey@gmail.com>
2025-06-05 13:05:29 +0530

    Adding CrossOrigin

--- Backend/src/main/java/com/example/Expensestracker/Config/WebConfig.java
+++ Backend/src/main/java/com/example/Expensestracker/Config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
         @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Apply CORS settings for all paths
-                .allowedOrigins("http://localhost:5173")  // Allow requests from your frontend
+                .allowedOrigins("http://localhost:5173") 
+                  .allowedOrigins("https://expense-tracker-ecru-sigma.vercel.app") 
+                // Allow requests from your frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow these methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow credentials like cookies (if necessary)
